73a11de45801007b207b7dfcab697bf56bdb80fe
Matthew Shin <mjyshin@gmail.com>
2018-06-15 18:37:00 -0700

    Add SVM, kNN, voting classifiers to main

--- run_charid.py
+++ run_charid.py
@@ -1,10 +1,15 @@
 import numpy as np
-import pandas as pd
 import time
 import matplotlib.pyplot as plt
 from load_data import load_data
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
 from sklearn.decomposition import PCA
-
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import VotingClassifier
 
 def dim_red(Xtrain, Xtest):
     """
@@ -24,6 +29,101 @@ def dim_red(Xtrain, Xtest):
     return {'Xtrain_red': Xtrain_red, 'Xtest_red': Xtest_red}
 
 
+def run_pca(Xtrain, ytrain, Xtest, ytest):
+    print("\n***** SUPPORT VECTOR MACHINE *****")
+    rbf_svm_best_mean = 0.
+    rbf_svm_best_param = [0., 0.]
+    cv_mean_acc = np.zeros((3, 10))
+    i = -1
+    for g in np.logspace(-3, -1, 3):
+        i = i + 1
+        j = -1
+        for c in np.linspace(3.5, 5, 10):
+            j = j + 1
+            rbf_svm_clf = Pipeline((
+                ("scaler", StandardScaler()),
+                ("svm_clf", SVC(kernel="rbf", gamma=g, C=c))
+            ))
+            time_start = time.clock()
+            cv_accuracies = cross_val_score(rbf_svm_clf, Xtrain, ytrain, cv=5, scoring="accuracy")
+            time_elapsed = time.clock() - time_start
+            cv_mean_acc[i, j] = np.mean(cv_accuracies)
+            print("CV mean = ", cv_mean_acc[i, j], ", CV training time = ", time_elapsed)
+            if cv_mean_acc[i, j] > rbf_svm_best_mean:
+                rbf_svm_best_mean = cv_mean_acc[i, j]
+                rbf_svm_best_param = [g, c]
+    # sio.savemat('rbf_svm_cv_mean_acc.mat', {'cv_mean_acc': cv_mean_acc})
+    print("CV results: gamma = ", rbf_svm_best_param[0], ", C = ", rbf_svm_best_param[1])
+    rbf_svm_best = Pipeline((
+        ("scaler", StandardScaler()),
+        ("svm_clf", SVC(kernel="rbf", gamma=rbf_svm_best_param[0], C=rbf_svm_best_param[1], probability=True))
+    ))
+    time_start = time.clock()
+    rbf_svm_best.fit(Xtrain, ytrain)
+    time_elapsed = time.clock() - time_start
+    ytrain_pred = rbf_svm_best.predict(Xtrain)
+    train_accuracy = accuracy_score(ytrain, ytrain_pred)
+    print("Training accuracy = ", train_accuracy, ", training time = ", time_elapsed)
+    ytest_pred = rbf_svm_best.predict(Xtest)
+    test_accuracy = accuracy_score(ytest, ytest_pred)
+    print("Test accuracy = ", test_accuracy)
+
+    return rbf_svm_best
+
+
+def run_knn(Xtrain, ytrain, Xtest, ytest):
+    print("\n***** K-NEAREST NEIGHBORS *****")
+    knn_best_mean = 0.
+    knn_best_param = ['uniform', 1]
+    cv_mean_acc = np.zeros((2, 10))
+    i = -1
+    for weigh in ['uniform', 'distance']:
+        i = i + 1
+        j = -1
+        for neigh in range(1, 11):
+            j = j + 1
+            knn_clf = KNeighborsClassifier(weights=weigh, n_neighbors=neigh)
+            time_start = time.clock()
+            cv_accuracies = cross_val_score(knn_clf, Xtrain, ytrain, cv=5, scoring="accuracy")
+            time_elapsed = time.clock() - time_start
+            cv_mean_acc[i, j] = np.mean(cv_accuracies)
+            print("CV mean accuracy = ", cv_mean_acc[i, j], ", CV training time = ", time_elapsed)
+            if cv_mean_acc[i, j] > knn_best_mean:
+                knn_best_mean = cv_mean_acc[i, j]
+                knn_best_param = [weigh, neigh]
+    # sio.savemat('knn_cv_mean_acc.mat', {'cv_mean_acc': cv_mean_acc})
+    print("CV results: weights = ", knn_best_param[0], ", n_neighbors = ", knn_best_param[1])
+    knn_best = KNeighborsClassifier(weights=knn_best_param[0], n_neighbors=knn_best_param[1])
+    time_start = time.clock()
+    knn_best.fit(Xtrain, ytrain)
+    time_elapsed = time.clock() - time_start
+    ytrain_pred = knn_best.predict(Xtrain)
+    train_accuracy = accuracy_score(ytrain, ytrain_pred)
+    print("Training accuracy = ", train_accuracy, ", training time = ", time_elapsed)
+    ytest_pred = knn_best.predict(Xtest)
+    test_accuracy = accuracy_score(ytest, ytest_pred)
+    print("Test accuracy = ", test_accuracy)
+
+    return knn_best
+
+
+def run_voting(Xtrain, ytrain, Xtest, ytest, rbf_svm_best, knn_best):
+    print("\n***** SOFT VOTING *****")
+    voting_clf = VotingClassifier(
+        estimators=[('svc', rbf_svm_best), ('knn', knn_best)],
+        voting='soft'
+    )
+    time_start = time.clock()
+    voting_clf.fit(Xtrain, ytrain)
+    time_elapsed = time.clock() - time_start
+    ytrain_pred = voting_clf.predict(Xtrain)
+    train_accuracy = accuracy_score(ytrain, ytrain_pred)
+    print("Training accuracy = ", train_accuracy, ", training time = ", time_elapsed)
+    ytest_pred = voting_clf.predict(Xtest)
+    test_accuracy = accuracy_score(ytest, ytest_pred)
+    print("Test accuracy = ", test_accuracy)
+
+
 def main():
     data = load_data()
     ytrain = data['ytrain']
@@ -34,6 +134,10 @@ def main():
     Xtrain = X_red['Xtrain_red']
     Xtest = X_red['Xtest_red']
     print(Xtest.shape)  # Size of reduced test dataset
+
+    rbf_svm_best = run_pca(Xtrain, ytrain, Xtest, ytest)
+    knn_best = run_knn(Xtrain, ytrain, Xtest, ytest)
+    run_voting(Xtrain, ytrain, Xtest, ytest, rbf_svm_best, knn_best)
 
 
 if __name__ == "__main__":
